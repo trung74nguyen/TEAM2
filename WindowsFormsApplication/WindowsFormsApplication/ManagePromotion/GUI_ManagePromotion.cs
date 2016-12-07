1e840a8fc7f380be676b11e649c2eeb5755c323f
toan lam <lamtantoan996@gmail.com>
2016-12-07 08:20:23 +0700

    Done GUI and Load data of PromotionInformation

--- WindowsFormsApplication/WindowsFormsApplication/ManagePromotion/GUI_ManagePromotion.cs
+++ WindowsFormsApplication/WindowsFormsApplication/ManagePromotion/GUI_ManagePromotion.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication.ManagePromotion
@@ -14,6 +15,26 @@ namespace WindowsFormsApplication.ManagePromotion
         public GUI_ManagePromotion()
         {
             InitializeComponent();
+        }
+        BUS_ManagePromotion bus = new BUS_ManagePromotion();
+        private void showManagePromotionForm(List<PromotionInformation> promotions)
+        {
+            lstManagePromotion.DataSource = promotions;
+            foreach (DataGridViewColumn column in lstManagePromotion.Columns)
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            lstManagePromotion.Columns["AccountCode"].HeaderText = "Mã khuyến mãi";
+            lstManagePromotion.Columns["ProductCode"].HeaderText = "Mã sản phẩm";
+            lstManagePromotion.Columns["PricePromotion"].HeaderText = "Giá khuyến mãi";
+            lstManagePromotion.Columns["StartTime"].HeaderText = "Thời gian bắt đầu";
+            lstManagePromotion.Columns["EndTime"].HeaderText = "Thời gian kết thúc";
+            lstManagePromotion.Columns["Cont"].HeaderText = "Nội dung";
+            lstManagePromotion.Columns["Image"].HeaderText = "Hình";
+            
+        }
+        private void showManagePromotionForm(object sender, EventArgs e)
+        {
+            var promotioninfo = bus.getAllListPromotion();
+            showManagePromotionForm(promotioninfo);
         }
     }
 }
